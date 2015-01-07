1d9fc2e215e914abe6b4111c1beaee5e0a4a891f
Chris Cotton <chris.cottonn@gmail.com>
2015-01-06 20:40:12 -0800

    added interactive grid and new grid button

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $("body").append("<div id='new_grid'><button type='button'>New Grid</button></div>");
+    $("body").append("<div class='container'></div>");
+    
+    newGrid(16);
+    
+    $("#new_grid").click(function() {
+        var length = prompt("How many squares per side?");
+        if (length != null) {
+            newGrid(length);
+        }
+    });
+});
+
+function newGrid(sideLength) {
+    $(".container").empty();
+    $(".container").css("width", "50%");
+    $(".container").prepend("<table></table>");
+    for (var i = 0; i < sideLength; i++) {
+            $("table").append("<tr></tr>");
+            
+            for (var j = 0; j < sideLength; j++) {
+                $("tr").eq(i).append("<td><div class='square'></div></td>");    
+            };
+    }; 
+        
+    $(".square").mouseenter(function() {
+        $(this).css("background-color", "blue");
+    });
+};
